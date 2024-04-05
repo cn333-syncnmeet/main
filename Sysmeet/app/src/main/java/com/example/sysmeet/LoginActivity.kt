7f906e6747062a314ebb3b56212fcfce206bb653
6410685256 <88659254+6410685256@users.noreply.github.com>
2024-04-05 13:49:01 +0700

    Navigation Bar

---
+++ Sysmeet/app/src/main/java/com/example/sysmeet/LoginActivity.kt
@@ -0,0 +1,60 @@
+package com.example.sysmeet
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sysmeet.databinding.ActivityLoginBinding
+import com.example.sysmeet.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Create.setOnClickListener {
+            val intent = Intent(this , SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.Login.setOnClickListener {
+            val email = binding.EmailAddress.text.toString()
+            val pass = binding.Password.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, task.exception?.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth = FirebaseAuth.getInstance() // อย่าลืมกำหนด firebaseAuth ให้ตัวแปรก่อนใช้งาน
+
+        if (firebaseAuth.currentUser != null && javaClass.name == LoginActivity::class.java.name) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+}
